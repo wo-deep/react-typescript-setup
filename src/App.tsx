25222dd7318f1c48e707ed3fe599fe1ca678327e
Deep <deep.webosmotic@gmail.com>
2021-02-14 16:37:49 +0530

    Init

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 function App(): React.ReactElement {
-    return <div>something stupid</div>;
+    return (
+        <div>
+            something stupid
+            <p>something</p>
+        </div>
+    );
 }
 
 export default App;
